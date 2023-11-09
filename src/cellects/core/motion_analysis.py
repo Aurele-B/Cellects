f0eb990cd4de15cc2e8537114e0663642b90dcc2
Aurele-B <aurele.boussard@gmail.com>
2023-11-09 21:04:04 +0100

    Add memory error print

--- src/cellects/core/motion_analysis.py
+++ src/cellects/core/motion_analysis.py
@@ -1572,7 +1572,10 @@ class MotionAnalysis:
             #    ampl_and_periods = ampl_and_periods[argsort(ampl_and_periods[:, 0])[-5:], :]
             #self.statistics["mean_amplitude_over_5_strongest"] = mean(ampl_and_periods[:, 0])
             #self.statistics["mean_period_over_5_strongest_in_min"] = mean(ampl_and_periods[:, 1] * self.time_interval)
-            oscillations_sign = sign(oscillations_sign)
+            try:
+                oscillations_sign = sign(oscillations_sign)
+            except np.core._exceptions._ArrayMemoryError:
+                logging.error("Not enough RAM. Retry without opening any program during analysis")
             mean_cluster_area = zeros(oscillations_sign.shape[0])
             cluster_number = zeros(oscillations_sign.shape[0])
             dotted_image = ones(self.converted_video.shape[1:3], uint8)
