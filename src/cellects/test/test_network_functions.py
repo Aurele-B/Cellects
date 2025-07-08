7c26b8199e1b744052e410ed422c5b9e2947fd2a
Aurele-B <aurele.boussard@gmail.com>
2025-07-08 18:42:44 +0200

    Add details for the network analysis

--- src/cellects/test/test_network_functions.py
+++ src/cellects/test/test_network_functions.py
@@ -223,8 +223,8 @@ def test_get_terminations_and_their_connected_nodes():
         [0, 0, 1, 0, 0, 0, 0, 1, 0, 0]], dtype=np.uint8)
     pad_skeleton = np.pad(skeleton, [(1,), (1,)], mode='constant')
     cnv4, cnv8 = get_neighbor_comparisons(pad_skeleton)
-    pad_terminations = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
-    vertices = pad_terminations[1:-1, 1:-1]
+    pad_tips = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
+    vertices = pad_tips[1:-1, 1:-1]
     target = np.array([
         [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -252,11 +252,21 @@ def test_get_terminations_and_their_connected_nodes():
     print(f"{test_name}{not np.array_equal(vertices, target)}")
 
 
-
-
-
-
-
+    test_nb += 1
+    test_name = f"Test {test_nb}, non connectivity: "
+    skeleton = np.array([
+        [1, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0]], dtype=np.uint8)
+    pad_skeleton = np.pad(skeleton, [(1,), (1,)], mode='constant')
+    cnv4, cnv8 = get_neighbor_comparisons(pad_skeleton)
+    pad_tips = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
+    nb, shape = cv2.connectedComponents(pad_tips)
+    print(f"{test_name}{pad_tips.sum() == nb - 1}")
 
 
 def test_get_inner_vertices():
@@ -500,10 +510,11 @@ def test_get_inner_vertices():
         [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 1, 0, 0]], dtype=np.uint8)
     pad_skeleton = np.pad(skeleton, [(1,), (1,)], mode='constant')
-    pad_skeleton = remove_small_loops(pad_skeleton)
+    pad_distances = pad_skeleton.copy()
+    pad_skeleton, distances = remove_small_loops(pad_skeleton, pad_distances)
     cnv4, cnv8 = get_neighbor_comparisons(pad_skeleton)
-    sure_terminations = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
-    pad_vertices = get_inner_vertices(sure_terminations, cnv8)
+    pad_tips = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
+    pad_vertices = get_inner_vertices(pad_tips, cnv8)
     vertices = pad_vertices[1:-1, 1:-1]
     target = np.array([
         [0, 0, 1, 1, 1, 0, 0, 1, 0, 0],
@@ -618,7 +629,6 @@ def test_remove_small_loops():
     target = skeleton.copy()
     print(f"{test_name}{np.array_equal(((numbered_vertices + numbered_edges) > 0).astype(np.uint8), target)}")
 
-
 def test_get_graph_from_vertices_and_edges():
     test_nb = 1
     test_name = f"Test {test_nb}, small loop: "
@@ -651,6 +661,50 @@ def test_get_graph_from_vertices_and_edges():
     numbered_vertices, numbered_edges, vertices_table, edges_labels = get_graph_from_vertices_and_edges(vertices, edges)
     target = skeleton.copy()
     print(f"{test_name}{np.array_equal(((numbered_vertices + numbered_edges) > 0).astype(np.uint8), target)}")
+
+
+def test_get_tipped_edges():
+    test_nb = 1
+    test_name = f"Test {test_nb}, bigger network: "
+    skeleton = np.array([
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 0]], dtype=np.uint8)
+    pad_skeleton = np.pad(skeleton, [(1,), (1,)], mode='constant')
+    pad_skeleton, pad_vertices, pad_tips = get_vertices_and_tips_from_skeleton(pad_skeleton)
+    nb_e, tempo_numbered_edges, numbered_branches, numbered_tips = get_numbered_edges_and_vertices(pad_skeleton, pad_vertices, pad_tips)
+
+
+
+    get_tipped_edges
+
+    pad_skeleton = np.array([
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 2, 0],
+        [0, 2, 1, 2, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 2, 2, 2],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 2, 0]], dtype=np.uint8)
+    skeleton = np.array([
+        [0, 0, 1, 1, 1, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1, 0]], dtype=np.uint8)
+
+
+    pad_distances = pad_skeleton.copy()
+    pad_skeleton, distances = remove_small_loops(pad_skeleton, pad_distances)
+    cnv4, cnv8 = get_neighbor_comparisons(pad_skeleton)
+    pad_tips = get_terminations_and_their_connected_nodes(pad_skeleton, cnv4, cnv8)
+    pad_vertices = get_inner_vertices(pad_tips, cnv8)
 
 
 
