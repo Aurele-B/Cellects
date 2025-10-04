57787c78121a757bf36d82e2a91db6a6f836ce88
manuelpett <manuel.petit@inria.fr>
2025-10-04 11:09:35 +0200

    Tests : Add coverage settings so it is possible to execute all the tests & compute the coverage using:
     coverage run -m unittest ; coverage report -m

--- tests/_base.py
+++ tests/_base.py
@@ -3,19 +3,10 @@ from __future__ import annotations
 from pathlib import Path
 import unittest
 
-def repo_root() -> Path:
-    return Path(__file__).resolve().parents[1]
-
-def tests_dir() -> Path:
-    return repo_root() / "tests"
-
-def data_dir() -> Path:
-    return repo_root() / "data"
-
 class CellectsUnitTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        d = data_dir()
+        d = Path(__file__).resolve().parents[1] / "data" # set up data path for the tests
         cls.path_input = d / "input"
         cls.path_output = d / "output"
         cls.path_experiment = d / "experiment"
