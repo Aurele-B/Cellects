acbee6ab3311b65cf282f8a83f7b825f3d10ae70
Aurele-B <aurele.boussard@gmail.com>
2025-12-02 10:46:52 +0100

    Create the ui_strings.py file to centralize all widgets user tips at the same place

---
+++ src/cellects/gui/ui_strings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+TOOLTIPS = {
+    "save_button": "Saves your current settings.",
+    "help_icon": "Click for detailed instructions on using this feature.",
+    "export_menu": "Exports data in CSV or PDF format."
+}
