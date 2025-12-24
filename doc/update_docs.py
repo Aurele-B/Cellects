7aac873f656cd17af7da0ba69b0499690b2964e7
Aurele-B <aurele.boussard@gmail.com>
2025-12-24 12:24:22 +0100

    Remove commented code

--- doc/update_docs.py
+++ doc/update_docs.py
@@ -7,7 +7,6 @@ from cellects.utils.utilitarian import vectorized_len
 from cellects.gui.ui_strings import *
 
 def wrap_tip(text, max_length=100):
-
     # Step 1: Insert newline before '-'
     modif_text = re.sub("-", "\n-", text)
 
@@ -20,16 +19,6 @@ def wrap_tip(text, max_length=100):
     for paragraph in split_text:
         wrapped_text += textwrap.fill(paragraph, width=max_length) + r"\n"
     wrapped_text = re.sub(r"\\n", "\n", wrapped_text)
-    # wrapped_text = re.sub("\n", r"\n", wrapped_text)
-
-    # split_text = wrapped_text.split("\n")
-    # wrapped_text = ""
-    # split_len = len(split_text)
-    # for _i, paragraph in enumerate(split_text):
-    #     if _i == split_len - 1:
-    #         wrapped_text += f'"{paragraph.strip()}"'
-    #     else:
-    #         wrapped_text += f'"{paragraph.strip()}"\n'
     return wrapped_text
 
 def process_tips_in_file(file_path):
@@ -70,7 +59,6 @@ def wrap_md_tip(text):
         if len(split_text) > 1:
             for paragraph in split_text[1:]:
                 wrapped_text += "\n\t -" + re.sub("\n", "", paragraph)# + "\n"
-        # split_text = np.array(modif_text.split("\n\t-"))
         final_note = "!!! note\n" + wrapped_text
     else:
         main = text
@@ -79,33 +67,9 @@ def wrap_md_tip(text):
         wrapped_main = split_text[0] + "\n-" + '-'.join(split_text[1:])
     else:
         wrapped_main = split_text[0]
-    # return main + final_note
     return wrapped_main + final_note
 
-# def wrap_md_tip(text):
-#     def replace_note(match):
-#         original = match.group(0)
-#         parts = original.split('\n', 1)  # Split into "NB:" line and the rest
-#         prefix_line = parts[0]
-#         rest = parts[1] if len(parts) > 1 else ''
-#
-#         new_prefix = '!!! note'
-#         indented_rest = re.sub(r'^\s*-\s*', r'    - ', rest, flags=re.MULTILINE)
-#
-#         return f'{new_prefix}\n{indented_rest}'
-#
-#     # Replace all occurrences of "NB:\n- ..." with the indented note block
-#     pattern = r'(NB:\n(?:.*?\n?)+?)'
-#     modified_text = re.sub(pattern, replace_note, text, flags=re.DOTALL)
-#
-#     # Ensure a line break before each `!!! note` if not already present and preceded by content
-#     modified_text = re.sub(r'([^\n])\s*(\n!!! note)', r'\1\n\2', modified_text)
-#
-#     return modified_text
-
 def update_markdown(file_path, dynamic_content):
-    #TODO: deal with list of items ("-") & admonitions ("!!! tip")
-
     # Generate dynamic content
     for key, value in dynamic_content.items():
         dynamic_lines = []
@@ -122,8 +86,6 @@ def update_markdown(file_path, dynamic_content):
             end_idx = lines.index(end_marker + "\n")
         except ValueError:
             raise RuntimeError(f"Markers not found in Markdown file: {start_marker} and {end_marker}")
-        # value = wrap_tip(value, 100)
-        # dynamic_lines.append(f"## {key.replace('_', ' ')}:\n{value}\n")
         modif_text = wrap_md_tip(value["tips"])
         dynamic_lines.append(f"## {value["label"]}:\n{modif_text}\n")
         # Replace the marker block
