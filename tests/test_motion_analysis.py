1045c4495ab75ccf714040b5a0d20906afa9cf9b
Aurele-B <aurele.boussard@gmail.com>
2026-02-07 21:58:31 +0100

    Add tests to MotionAnalysis

--- tests/test_motion_analysis.py
+++ tests/test_motion_analysis.py
@@ -17,7 +17,7 @@ class TestFullMotionAnalysis(CellectsUnitTest):
     def setUpClass(cls):
         super().setUpClass()
         os.chdir(cls.path_output)
-        cls.color_space_combination = {"logical": 'None', "PCA": np.ones(3, dtype=np.uint8)}
+        cls.color_space_combination = {"logical": 'None', "PCA": [1, 1, 1]}
         cls.videos_already_in_ram = [rgb_video_test[:, :, :, :], rgb_video_test[:, :, :, 0]]
         cls.i = 0
         cls.vars = DefaultDicts().vars
@@ -37,7 +37,7 @@ class TestMotionAnalysisWithOneFrame(CellectsUnitTest):
     def setUpClass(cls):
         super().setUpClass()
         os.chdir(cls.path_output)
-        cls.color_space_combination = {"logical": 'None', "PCA": np.ones(3, dtype=np.uint8)}
+        cls.color_space_combination = {"logical": 'None', "PCA": [1, 1, 1]}
         cls.videos_already_in_ram = [rgb_video_test[0, :, :, :][None, :, :, :], rgb_video_test[0, :, :, 0][None, :, :]]
         cls.i = 0
         cls.vars = DefaultDicts().vars
@@ -75,7 +75,7 @@ class TestMotionAnalysisWithMP4(CellectsUnitTest):
         write_h5(f'ind_{1}.h5', self.bin_image, 'origin_coord')
         self.vars['video_list'] = ['video.mp4']
         image = readim('image1.tif')
-        self.color_space_combination = {"logical": 'None', "PCA": np.ones(3, dtype=np.uint8)}
+        self.color_space_combination = {"logical": 'None', "PCA": [1, 1, 1]}
         self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
         self.ma = MotionAnalysis(self.l)
 
@@ -104,7 +104,7 @@ class TestMotionAnalysisWithOneBlob(CellectsUnitTest):
     def setUpClass(cls):
         super().setUpClass()
         os.chdir(cls.path_output)
-        cls.color_space_combination = {"logical": 'None', "PCA": np.ones(3, dtype=np.uint8)}
+        cls.color_space_combination = {"logical": 'None', "PCA": [1, 1, 1]}
         cls.videos_already_in_ram = rgb_video_test[:, :, :, 0]
         cls.i = 0
         cls.dd = DefaultDicts()
@@ -142,8 +142,7 @@ class TestMotionAnalysisWithOneBlob(CellectsUnitTest):
         self.ma.get_covering_duration(1)
 
 class TestMotionAnalysisSlopeDetection(TestMotionAnalysisWithOneBlob):
-    """Test suite for OneImageAnalysis class"""
-
+    """Test suite for MotionAnalysis class"""
 
     def test_most_central_detection(self):
         self.ma = MotionAnalysis(self.l)
@@ -153,20 +152,14 @@ class TestMotionAnalysisSlopeDetection(TestMotionAnalysisWithOneBlob):
         self.ma.get_covering_duration(1) # Put this in init
         self.assertGreater(self.ma.substantial_time, 0)
 
-    def test_detect_slope_only(self):
-        self.ma = MotionAnalysis(self.l)
-        self.ma.get_origin_shape()
-        self.ma.get_covering_duration(1)
-        self.ma.vars['do_slope_segmentation'] = True
-        self.ma.vars['do_value_segmentation'] = False
-        self.ma.vars['frame_by_frame_segmentation'] = False
-        self.ma.detection(False)
-        self.assertTrue(np.all(self.ma.segmented.sum((1,2))))
-
 class TestMotionAnalysisAnd(TestMotionAnalysisWithOneBlob):
-    """Test suite for OneImageAnalysis class"""
+    """Test suite for MotionAnalysis class"""
 
     def test_detect_slope_and_thresh(self):
+        self.vars['lose_accuracy_to_save_memory'] = True
+        self.vars['repeat_video_smoothing'] = 2
+        self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
+        self.ma = MotionAnalysis(self.l)
         self.ma = MotionAnalysis(self.l)
         self.ma.get_origin_shape()
         self.ma.get_covering_duration(1)
@@ -178,9 +171,12 @@ class TestMotionAnalysisAnd(TestMotionAnalysisWithOneBlob):
         self.assertTrue(np.any(self.ma.segmented.sum((1,2))))
 
 class TestMotionAnalysisOr(TestMotionAnalysisWithOneBlob):
-    """Test suite for OneImageAnalysis class"""
+    """Test suite for MotionAnalysis class"""
 
     def test_detect_slope_or_thresh(self):
+        self.vars['lose_accuracy_to_save_memory'] = False
+        self.vars['repeat_video_smoothing'] = 2
+        self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
         self.ma = MotionAnalysis(self.l)
         self.ma.get_origin_shape()
         self.ma.get_covering_duration(1)
@@ -192,7 +188,7 @@ class TestMotionAnalysisOr(TestMotionAnalysisWithOneBlob):
         self.assertTrue(np.all(self.ma.segmented.sum((1,2))))
 
 class TestMotionAnalysisStep1(TestMotionAnalysisWithOneBlob):
-    """Test suite for OneImageAnalysis class"""
+    """Test suite for MotionAnalysis class"""
 
     def test_detect_frame_step1(self):
         self.ma = MotionAnalysis(self.l)
@@ -205,8 +201,92 @@ class TestMotionAnalysisStep1(TestMotionAnalysisWithOneBlob):
         self.ma.detection(False)
         self.assertTrue(np.all(self.ma.segmented.sum((1,2))))
 
+
+class TestMotionAnalysisWith2Conversions(CellectsUnitTest):
+    """Test suite for MotionAnalysis with 2 conversions"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        os.chdir(cls.path_output)
+        cls.videos_already_in_ram = rgb_video_test[:, :, :, :], rgb_video_test[:, :, :, 0], rgb_video_test[:, :, :, 2]
+        cls.i = 0
+        cls.dd = DefaultDicts()
+        cls.vars = cls.dd.vars
+        for k in cls.vars['descriptors'].keys():
+            cls.vars['descriptors'][k] = True
+        write_h5(f'ind_{1}.h5', np.array(np.nonzero(binary_video_test[0])), 'origin_coord')
+        cls.vars['already_greyscale'] = False
+        cls.vars['drift_already_corrected'] = True # Will automatically become False
+        cls.vars['lose_accuracy_to_save_memory'] = False
+        cls.vars['first_move_threshold'] = 1
+        cls.vars['lighter_background'] = False
+        cls.vars['several_blob_per_arena'] = False
+        cls.vars['output_in_mm'] = True
+        cls.vars['do_fading'] = True
+        cls.vars['save_coord_network'] = True
+        cls.vars['save_graph'] = True
+        cls.vars['oscilacyto_analysis'] = True
+        cls.vars['save_coord_thickening_slimming'] = True
+        cls.vars['fractal_analysis'] = True
+        cls.vars['save_processed_videos'] = True
+        cls.vars['filter_spec'] = None
+        cls.vars['correct_errors_around_initial'] = True
+        cls.vars['prevent_fast_growth_near_periphery'] = True
+        cls.vars['origin_state'] = "constant"
+        cls.vars['average_pixel_size'] = 1
+        cls.vars['save_coord_specimen'] = False
+        cls.vars["appearance_detection_method"] = 'largest'
+        cls.vars['contour_color']: np.uint8 = 0
+
+    def test_detect_slope_with_or(self):
+        self.vars['convert_for_motion'] = {"logical": 'Or', "lab": [0, 0, 1], "luv2": [0, 0, 1]}
+        self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
+        self.ma = MotionAnalysis(self.l)
+        self.ma.get_origin_shape()
+        self.ma.get_covering_duration(1)
+        self.ma.vars['do_slope_segmentation'] = True
+        self.ma.vars['do_value_segmentation'] = False
+        self.ma.vars['frame_by_frame_segmentation'] = False
+        self.ma.detection(False)
+        self.assertTrue(isinstance(self.ma.segmented, np.ndarray))
+
+    def test_detect_slope_with_and(self):
+        self.vars['convert_for_motion'] = {"logical": 'And', "lab": [0, 0, 1], "luv2": [0, 0, 1]}
+        self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
+        self.ma = MotionAnalysis(self.l)
+        self.ma.get_origin_shape()
+        self.ma.get_covering_duration(1)
+        self.ma.vars['do_slope_segmentation'] = True
+        self.ma.vars['do_value_segmentation'] = False
+        self.ma.vars['frame_by_frame_segmentation'] = False
+        self.ma.detection(False)
+        self.assertTrue(isinstance(self.ma.segmented, np.ndarray))
+
+    def test_detect_slope_with_xor(self):
+        self.vars['convert_for_motion'] = {"logical": 'Xor', "lab": [0, 0, 1], "luv2": [0, 0, 1]}
+        self.l = [self.i, self.i + 1, self.vars, False, False, False, self.videos_already_in_ram]
+        self.ma = MotionAnalysis(self.l)
+        self.ma.get_origin_shape()
+        self.ma.get_covering_duration(1)
+        self.ma.vars['do_slope_segmentation'] = True
+        self.ma.vars['do_value_segmentation'] = False
+        self.ma.vars['frame_by_frame_segmentation'] = False
+        self.ma.detection(False)
+        self.assertTrue(isinstance(self.ma.segmented, np.ndarray))
+
+    # def test_detect_thresh_only(self):
+    #     self.ma = MotionAnalysis(self.l)
+    #     self.ma.get_origin_shape()
+    #     self.ma.get_covering_duration(1)
+    #     self.ma.vars['do_slope_segmentation'] = True
+    #     self.ma.vars['do_value_segmentation'] = False
+    #     self.ma.vars['frame_by_frame_segmentation'] = False
+    #     self.ma.detection(False)
+    #     self.assertTrue(np.all(self.ma.segmented.sum((1,2))))
+
 class TestMotionAnalysisFullPipeline(TestMotionAnalysisWithOneBlob):
-    """Test suite for OneImageAnalysis class"""
+    """Test suite for MotionAnalysis class"""
     def test_post_processing(self):
         write_h5(f'ind_{1}.h5', np.array(np.nonzero(binary_video_test[0])), 'origin_coord')
         self.ma = MotionAnalysis(self.l)
@@ -241,7 +321,7 @@ class TestMotionAnalysisFullPipeline(TestMotionAnalysisWithOneBlob):
 
 
 class TestMotionAnalysisWithSeveralBlob(CellectsUnitTest):
-    """Test suite for OneImageAnalysis class"""
+    """Test suite for MotionAnalysis class"""
 
     @classmethod
     def setUpClass(cls):
