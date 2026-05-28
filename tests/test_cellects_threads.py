5813ab17cebab4646585247e95ebad02aad65997
Aurele-B <aurele.boussard@gmail.com>
2026-05-28 15:59:08 +0200

    Replace explicit file deletions in test teardowns with `insensitive_glob` wildcard cleanup, simplifying and consolidating file removal logic.

--- tests/test_cellects_threads.py
+++ tests/test_cellects_threads.py
@@ -214,7 +214,7 @@ class TestCellectsThreads(CellectsUnitTest):
         """Remove all written files."""
         if os.path.isfile("cellects_settings.json"):
             os.remove("cellects_settings.json")
-        files_to_remove = insensitive_glob('*.h5') + insensitive_glob('Analysis efficiency*') + insensitive_glob('*.csv') + insensitive_glob('ind_1.mp4') + insensitive_glob('edges_*') + insensitive_glob('vertices_*')
+        files_to_remove = insensitive_glob('*.h5') + insensitive_glob('Analysis efficiency*') + insensitive_glob('*.csv') + insensitive_glob('ind_1.mp4')
         for file in files_to_remove:
             if os.path.isfile(file):
                 os.remove(file)
